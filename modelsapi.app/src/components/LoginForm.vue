41b6d863317524ccd8b1f584da37b6a9fdfe4aff
GitDal <jeppedybdal@gmail.com>
2020-05-15 18:11:27 +0200

    Login done

---
+++ modelsapi.app/src/components/LoginForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="loginForm">
+    <section class="form">
+        <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+                <input v-model="username" class="input" type="text" placeholder="Enter Email">
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+                <input v-model="password" class="input" type="text" placeholder="Enter Password">
+            </div>
+        </div>
+        <div class="field">
+            <button @click="formSubmit">Login</button>
+        </div>
+    </section>
+    <p>{{statusMsg}}</p>
+
+    </div>
+</template>
+
+<script>export default {
+name: 'LoginForm',
+    data(){
+        return{
+        username: "",
+        password: "",
+        statusMsg: ""
+        }
+    },
+    methods:{
+        formSubmit(){
+            fetch("https://localhost:44368/api/Account/login", {
+                method: 'POST',
+                body: JSON.stringify({
+                      email: this.username,
+                       password: this.password}),
+                headers: new Headers({
+                    'Content-Type': 'application/json'})})
+            .then(res => res.json())
+            .then((token) => localStorage.setItem("token", token.jwt))
+            .catch(error => console.error('Error:', error));
+        }
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- modelsapi.app/src/views/Login.vue
+++ modelsapi.app/src/views/Login.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="login">
         <h1>This is the login page</h1>
+        <LoginForm/>
     </div>
 </template>
 
+<script>
+    // @ is an alias to /src
+    import LoginForm from '@/components/LoginForm.vue'
+
+    export default {
+        name: 'Login',
+        components: {
+            LoginForm
+        }
+    }
+</script>
