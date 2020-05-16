7af5ec9414d9f7ae55c1d3b3cf5dd22d0a5173b7
GitDal <jeppedybdal@gmail.com>
2020-05-16 16:29:10 +0200

    Her

---
+++ modelsapi.app/src/components/ModelsList.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+    <div v-if="isLoading">
+        <p>Loading content..</p>
+    </div>
+    <div v-for="model in models" :key="model.efModelId">
+        <p>{{model.firstname}}</p>
+    </div>
+</div>
+</template>
+
+<script>export default {
+name: 'ModelsList',
+    data(){
+        return{
+        isLoading: true,
+        message: '',
+        models: []
+        }
+    },
+    methods:{
+        GetManagers(){
+            // fetch('https://localhost:44368/api/Models', { 
+            //     method: 'GET',
+            //     credentials: 'include',
+            //     headers: {
+            //         'Authorization': 'Bearer ' + localStorage.getItem("token"),
+            //         'Content-Type': 'application/json' 
+            //         }})
+            // .then(responseJson => { 
+            //     //var items = JSON.parse(responseJson);
+            //     //this.models = items;
+            //     this.models = responseJson;
+            //     this.isLoading = false;
+            // })
+            // .catch(error => {
+            //     this.isLoading = false;
+            //     this.message = 'Something bad happened ' + error;
+            // });
+            fetch('https://localhost:44368/api/Models', { 
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                    'Content-Type': 'application/json' 
+                    }})
+            .then(res => res.json())
+            .then(responseJson => { 
+                this.models = responseJson;
+            })
+            .catch(error => {
+                this.isLoading = false;
+                this.message = 'Something bad happened ' + error;
+            });
+        }
+    },
+    mounted(){
+        this.GetManagers();
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- modelsapi.app/src/views/Manager.vue
+++ modelsapi.app/src/views/Manager.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="manager">
-    <h1>This is an about page</h1>
+    <h1>Manager page</h1>
+    <ModelsList />
   </div>
 </template>
+
+<script>
+    // @ is an alias to /src
+    import ModelsList from '@/components/ModelsList.vue'
+
+    export default {
+        name: 'Manager',
+        components: {
+            ModelsList
+        }
+    }
+</script>
